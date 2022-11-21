50e7602f8a3147ffbd76995b4f34b18f296cd2cc
blessymerlin <118697524+blessymerlin@users.noreply.github.com>
2022-11-21 17:03:23 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,16 @@
+WEBSITE CREATION
+Overview
+This project aims to create a crawl website and save the images and text from the website.
+Context
+Problem
+1. Creating a scrolling website.
+2. Creation of template page for images and texts.
+3. Creation of a media folder to store images.
+Design
+To create a website using Django
+1. Create a virtual environment for the project. Create a project in the environment.
+2. Create model for database creation, creation of views for functionality.
+3. Link the views to the url
+4. Create a template for display and take user inputs
+5. Create a media folder for storing images
+6. Run the server to see the website .
